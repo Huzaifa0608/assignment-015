982a20af64b7b4d469d3a35821a5f817ba5b7236
Huzaifa0608 <huzaifafarhan7890@gmail.com>
2023-03-31 23:39:32 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,201 @@
+alert("Thanks for your input!");
+window.alert("Thanks for your input!");
+var f_name = "Mark";
+alert  (f_name);
+f_name = "are";
+alert(f_name);
+var nationality;
+nationality = "U.S.";
+alert(nationality);
+
+alert("practise of meaningful name");
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+
+alert("practise sting");
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+
+var weight = 150;
+var num = weight + 25;
+alert(num);
+
+var originalNum = 23;
+var newNum = originalNum + 7;
+alert(newNum);
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+
+alert(newNum);
+
+
+var originalNum = 90;
+originalNum = originalNum + 10;
+alert(originalNum);
+
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+alert(newNum);
+
+var originalNumber = 23;
+var newNumber = originalNumber + 7;
+alert(newNumber);
+
+alert("illegel synatax");
+// var 1stPresident = "Washington";
+// alert(stPresident)
+alert("legel synatax");
+var prezWhoCame1st = "Washington";
+alert(144);
+var caseQty = 144;
+alert(caseQty);
+
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+alert(2 + 2);
+var popularNumber = 12 - 24;
+var popularNumber = 3 * 12;
+var num = 10;
+var popularNumber = num + 200;
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+
+var whatsLeftOver = 10 % 3;
+
+var whatsLeftOver = 9 % 3;
+
+num++;
+num = num + 1;
+num--;
+alert(num);
+
+var num = 1;
+var newNum = num++;
+alert(newNum);
+
+var num = 1;
+var newNum = ++num;
+alert(newNum);
+
+var num = 1;
+var newNum = num--;
+alert(newNum);
+
+var num = 1;
+var newNum = --num;
+alert(newNum);
+
+var resultOfComputation;
+resultOfComputation = ((2 * 4) * 4) + 2;
+alert(resultOfComputation);
+resultOfComputation = (2 * 4) * (4 + 2);
+alert(resultOfComputation);
+
+var message = "Thanks for your input!";
+ alert(message);
+ alert("Thanks, " + userName + "!");
+
+var message = "Thanks, ";
+var banger = "!";
+alert(message + userName + banger);
+
+alert("Thanks, " + "Susan" + "!");
+
+var message = "Thanks, ";
+var userName = "Susan";
+var banger = "!";
+var customMess = message + userName + banger;
+alert(customMess);
+
+alert("2" + "2");
+
+alert("2 plus 2 equals " + 2 + 2);
+
+var spec = prompt("Your species?", "human");
+
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats + 1;
+
+var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+alert("Correct!");
+}
+
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+alert("Correct!");
+}
+
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+score++;
+userIQ = "genius";
+alert("Correct!");
+}
+
+// if (fullName === ("Mark" + " " + "Myers")) {
+// }
+    // if (fullName === firstName + " " + "Myers") {
+    // }
+    // if (fullName === firstName + " " + "Myers") {
+    // }
+    // if (fullName === ("firstName" + " " + "lastName")) {
+    // }
+    // if (totalCost === 81.50 + 135) {
+    // }
+    // if (totalCost === materialsCost + 135) {
+    // }
+    // if (totalCost === materialsCost + laborCost) {}
+        
+    // if (x + y === a - b){ }
+
+   
+    //  if (yourTicketNumber !== 487208) {
+    //      alert("Better luck next time.");
+    //      }
+
+ var x = prompt("Where does the Pope live?");
+ if (x === "Vatican") {
+ alert("Correct!");
+ }
+
+ var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+alert("Correct!");
+}
+if (x !== "Vatican") {
+alert("Wrong answer");
+}
+
+if (x === "Vatican") {
+ alert("Correct!");
+ }
+ else {
+ alert("Wrong answer");
+ 
+}
+
+var correctAnswer = "Vatican";
+if (x === correctAnswer) {
+alert("Correct!");
+}
+else {
+score--;
+userIQ = "problematic";
+alert("Incorrect");
+}
+    
+
+
+        
